814b4e3db97abfcb86c2c4061241c4b64c8c33c3
abishek0057 <abishekparajuli64@gmail.com>
2023-08-03 21:26:59 +0545

    add functionality to delete single product and its image on cloudinary

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,12 +3,13 @@ const Product = require("../models/productModel");
 const { fileSizeFormatter } = require("../utils/fileUpload");
 const cloudinary = require("cloudinary").v2;
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const createProduct = async (req, res, next) => {
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
   try {
     const { name, sku, category, quantity, price, description } = req.body;
     if (!name || !category || !quantity || !price || !description) {
@@ -29,10 +30,11 @@ const createProduct = async (req, res, next) => {
         throw new Error("Image could not be uploaded");
       }
       fileData = {
-        fileName: req.file.Originalname,
+        fileName: uploadedFile.Original_filename,
         filePath: uploadedFile.secure_url,
         fileType: req.file.mimetype,
         fileSize: fileSizeFormatter(req.file.size, 2),
+        publicId: uploadedFile.public_id,
       };
     }
 
@@ -87,8 +89,35 @@ const getProduct = async (req, res, next) => {
   }
 };
 
+const deleteProduct = async (req, res, next) => {
+  try {
+    const productId = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(productId)) {
+      res.status(400);
+      throw new Error("Invalid product ID");
+    }
+
+    const product = await Product.findById(productId);
+    if (!product) {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+    if (product.user.toString() !== req.user.id) {
+      res.status(404);
+      throw new Error("User not authorized");
+    }
+    if (product.image.publicId)
+      await cloudinary.uploader.destroy(product.image.publicId);
+    const delProduct = await Product.deleteOne({ _id: productId });
+    res.status(200).json({ delProduct });
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   createProduct,
   getProducts,
   getProduct,
+  deleteProduct,
 };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,6 +4,7 @@ const {
   createProduct,
   getProducts,
   getProduct,
+  deleteProduct,
 } = require("../controllers/productController");
 const { upload } = require("../utils/fileUpload");
 
@@ -12,5 +13,6 @@ const router = express.Router();
 router.post("/", protect, upload.single("image"), createProduct);
 router.get("/", protect, getProducts);
 router.get("/:id", protect, getProduct);
+router.delete("/:id", protect, deleteProduct);
 
 module.exports = router;
